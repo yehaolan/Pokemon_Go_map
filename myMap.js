0dbcdec6757852182d28a4a5816abd5b9179d533
Haolan Ye <yehaolan@gmail.com>
2016-10-22 16:48:12 -0700

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,6 +1,6 @@
 var map;
 function loadMapScenario() {
     map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-        credentials: 'Your Bing Maps Key'
+        credentials: 'AvOWU65g2gmhFTaF_8m3H3ZYUAD0IYD6BC5jvc-HbtfQvutR9SB2S0cPlQRbTqTR'
     });
 }
