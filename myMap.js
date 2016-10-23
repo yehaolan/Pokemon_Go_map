ad5c4b7e121b7c2b5ce8391922d42cd47d84c259
Haolan Ye <yehaolan@gmail.com>
2016-10-23 01:39:34 -0700

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -76,8 +76,8 @@ function refresh_pokemon_data() {
     apigClient.mapPokemonsGet(params, body, additionalParams)
         .then(function(result){
             //This is where you would put a success callback
-//             map_manager.map_items = result.data;
-            console.log(result);
+            map_manager.map_items = result.data;
+//             console.log(result);
         }).catch( function(result){
             //This is where you would put an error callback
             console.log(result);
