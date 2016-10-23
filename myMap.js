6416d59ee0a0af3f40f3e1ece45ae87fce376a87
Haolan Ye <yehaolan@gmail.com>
2016-10-22 23:17:16 -0700

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -43,8 +43,16 @@ function add_pokemon_layer() {
     map.layers.insert(pokemon_layer);
 }
 
-
 // 3. Add pokemon counter down refresh
+function refresh_pokemon_layer() {
+    //Prepare new Layer
+    var pokemon_layer = get_pokemon_layer_from_map_items(map_items);
+    //Remove old layer
+    map.layers.clear();
+    //Add new layer
+    map.layers.insert(pokemon_layer);
+}
+window.setInterval(refresh_pokemon_layer, 1000);
 
 // 4. Connect with REST API
 
