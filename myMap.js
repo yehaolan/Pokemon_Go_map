6cdcecd29f520df41c774800b631a1af820b1d4b
Haolan Ye <yehaolan@gmail.com>
2016-10-22 18:55:10 -0700

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -3,6 +3,7 @@ function loadMapScenario() {
     map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: 'AvOWU65g2gmhFTaF_8m3H3ZYUAD0IYD6BC5jvc-HbtfQvutR9SB2S0cPlQRbTqTR'
     });
+    add_pokemon_layer();
 }
 
 // 1. Define pokemon data format, create mock pokemon data
@@ -21,11 +22,12 @@ function get_pokemon_layer_from_map_items(map_items) {
     var pushpins = Microsoft.Maps.TestDataGenerator.getPushpins(10, map.getBounds());
     layer.add(pushpins);
     return layer;
-    
-    
 }
-var pokemon_layer = get_pokemon_layer_from_map_items(map_items);
-map.layers.insert(layer);
+function add_pokemon_layer() {
+    var pokemon_layer = get_pokemon_layer_from_map_items(map_items);
+    map.layers.insert(layer);
+}
+
 
 // 3. Add pokemon counter down refresh
 
